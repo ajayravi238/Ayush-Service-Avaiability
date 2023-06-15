e0649a719538e48455df3617b01c56e50cc17dd5
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2023-06-15 20:01:41 +0530

    Add files via upload

---
+++ AYUSH SERVICE AVAILABILITY/AYUSH SERVICE AVAILABILITY/js/covid-hospitals-data.js
@@ -0,0 +1,196 @@
+let hospitals = [{
+    "name": "Tirunelveli MultiSpeciality Hospital",
+    "phoneNumber": "0462 253732",
+    "openTime": "Open 24 Hours",
+    "coordinates": {
+        "latitude": 8.714486350000001,
+        "longitude": 77.75826180502739,
+    },
+
+    "address": "Maharaja Nagar",
+    "addressLines": [
+        "Maharaja Nagar Tirunelveli"],
+    "pincode": "627011",
+    "zipCode": "627011", /* first few digits */
+    "numBeds": "513",
+    "rating": "3.6",
+},
+
+{
+    "name": "galaxy hospitals",
+    "phoneNumber": "0462 253732",
+    "openTime": "Open 24 Hours",
+    "coordinates": {
+        "latitude": 8.7284606,
+        "longitude": 77.7101795,
+    },
+
+    "address": "Junction",
+    "addressLines": [
+        "Maharaja Nagar Tirunelveli"],
+    "pincode": "627011",
+    "zipCode": "627011", /* first few digits */
+    "numBeds": "52",
+    "rating": "3.6",
+},
+{
+    "name": "Al Shifa Ayush Hospital",
+    "phoneNumber": "090422 25333",
+    "openTime": "Monday-Saturday(7:29am-9.30pm),Sunday(8am-9pm)",
+    "coordinates": {
+        "latitude": 9.0859409,
+        "longitude": 77.3475927,
+    },
+
+    "address": "Kadayanallur",
+    "addressLines": [
+        "11, Main Rd, Krishnapuram, Kadayanallur, Tamil Nadu "],
+    "pincode": "627751",
+    "zipCode": "627751", /* first few digits */
+    "numBeds": "52",
+    "rating": "4.8",
+},
+
+{
+    "name": "Al Shifa Ayush Hospital-Tirunelveli",
+    "phoneNumber": "09042225999",
+    "openTime": "Monday-Saturday(10am-8pm),Sunday(10am-7pm)",
+    "coordinates": {
+        "latitude": 8.712506297919852,
+        "longitude": 77.73163083407064,
+    },
+
+    "address": "Tirunelveli",
+    "addressLines": [
+        "30, Trivandrum Rd, Kattnayakan, Palayamkottai"],
+    "pincode": "627007",
+    "zipCode": "627007", /* first few digits */
+    "numBeds": "52",
+    "rating": "4.8",
+},
+{
+    "name": "Jai Ayush Siddha Hospital",
+    "phoneNumber": "Not Available",
+    "openTime": "Monday-Saturday(10am-8pm),Sunday(10am-7pm)",
+    "coordinates": {
+        "latitude": 8.718770203431115,
+        "longitude": 77.7330345545707,
+    },
+
+    "address": "Tirunelveli",
+    "addressLines": [
+        "Bus stand, NO 1 /1c Thulasi Valagam, Thiruchendur, N Main Rd, opposite Tirunelveli, Palayamkottai"],
+    "pincode": "627002",
+    "zipCode": "627002", /* first few digits */
+    "numBeds": "52",
+    "rating": "5.0",
+},
+{
+    "name": "AAROGYA AYURVEDA HOSPITAL",
+    "phoneNumber": "Not Available",
+    "openTime": "Monday-Saturday(10am-8pm),Sunday(10am-7pm)",
+    "coordinates": {
+        "latitude": 8.961650570415639,
+        "longitude":77.30310162010349,
+    },
+
+    "address": "Tenkasi",
+    "addressLines": [
+        "Elanji, road Tenkasi"],
+    "pincode": "627811",
+    "zipCode": "627811", /* first few digits */
+    "numBeds": "52",
+    "rating": "5.0",
+},
+{
+    "name": "Nisha Siddha Hospital",
+    "phoneNumber": "Not Available",
+    "openTime": "Monday-Saturday(10am-8pm),Sunday(10am-7pm)",
+    "coordinates": {
+        "latitude": 8.789392006265631, 
+        "longitude":78.13118639332832,
+    },
+
+    "address": "Thoothukudi",
+    "addressLines": [
+        "No: 1/1A, 9th Middle Street, Near Rassi Kalyana Mandapam, Bryant Nagar, Thoothukudi"],
+    "pincode": "628008",
+    "zipCode": "628008", /* first few digits */
+    "numBeds": "52",
+    "rating": "3.5",
+},
+{
+    "name": "SRI KRISHNA AYURVEDA CHIKITSALAYA",
+    "phoneNumber": "Not Available",
+    "openTime": "Monday-Saturday(10am-8pm),Sunday(Closed)",
+    "coordinates": {
+        "latitude": 8.795740190995323, 
+        "longitude":78.12073634747671,
+    },
+
+    "address": "Thoothukudi",
+    "addressLines": [
+        "Q4WC+77Q, 6th St E, Teachers Colony, Sinnamani Nagar, Thoothukudi"],
+    "pincode": "628008",
+    "zipCode": "628008", /* first few digits */
+    "numBeds": "52",
+    "rating": "5.0",
+},
+
+{
+    "name": "Shalo ayurveda hospital",
+    "phoneNumber": "Not Available",
+    "openTime": "Monday-Saturday(10am-8pm),Sunday(Closed)",
+    "coordinates": {
+        "latitude": 9.182144240464787, 
+        "longitude":77.87526997959569,
+    },
+
+    "address": "Kovilpatti",
+    "addressLines": [
+        "66, Sevvalpatti, Kovilpatti"],
+    "pincode": "628502",
+    "zipCode": "628502", /* first few digits */
+    "numBeds": "52",
+    "rating": "No Reviews",
+},
+
+{
+    "name": "Aruna Siddha Marundhagam",
+    "phoneNumber": "Not Available",
+    "openTime": "Monday-Saturday(10am-8pm),Sunday(Closed)",
+    "coordinates": {
+        "latitude":9.174968697980132, 
+        "longitude":77.86646101827716,
+    },
+
+    "address": "Kovilpatti",
+    "addressLines": [
+        "5VF8+XHP, Santhai Pettai Street, V.O.C. Nagar, Kadershan Koil, Kovilpatti"],
+    "pincode": "628501",
+    "zipCode": "628501", /* first few digits */
+    "numBeds": "52",
+    "rating": "4.5",
+},
+
+{
+    "name": "AL-SHIFA AYUSH HOSPITAL & Kottakkal Arya vaidya sala -Rajapalayam",
+    "phoneNumber": "Not Available",
+    "openTime": "Monday-Saturday(10am-8pm),Sunday(10am-8.30pm)",
+    "coordinates": {
+        "latitude":9.441961381349135, 
+        "longitude":77.56030978027523,
+    },
+
+    "address": "Rajapalayam",
+    "addressLines": [
+        "#195, PAC Ramasamy Raja Salai, Thoppupatti, Rajapalayam"],
+    "pincode": "626117",
+    "zipCode": "626117", /* first few digits */
+    "numBeds": "52",
+    "rating": "4.8",
+},
+
+]
+
+
